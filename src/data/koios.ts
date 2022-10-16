e788fb75b02b0ec4e1406d9f88284d38941e09c3
jack-0 <github.ay8yf@slmail.me>
2022-10-16 18:20:26 +0100

    init with svelte and ts

---
+++ src/data/koios.ts
@@ -0,0 +1,21 @@
+import type { Delegator } from "src/types/koios";
+
+const ApiUrl = "https://api.koios.rest/api/v0";
+
+export async function getDelegators(
+  poolBech32: string = "pool1fu6ppur5uumrpydpeswzrvfg4epr68xw39aar9rcu56tk5ukat3"
+): Promise<Delegator[]> {
+  const res = await fetch(`${ApiUrl}/pool_delegators`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      _pool_bech32: poolBech32,
+    }),
+  }).then((res) => {
+    return res.json();
+  });
+
+  return res;
+}
