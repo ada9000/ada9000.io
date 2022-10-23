42633dddc05941e8b4210c4491ae362c59a98d32
jack-0 <github.ay8yf@slmail.me>
2022-10-23 13:55:29 +0100

    updated site

---
+++ src/components/footer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  const year = new Date().getFullYear();
+</script>
+
+<footer class="container mx-auto flex w-full p-4">
+  <div class="items-center mx-auto">
+    <p>©{year} ada9000.io</p>
+  </div> 
+</footer>
