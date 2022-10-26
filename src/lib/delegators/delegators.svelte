d4b318d07512918bf69ad9b56b2162963c5d0edb
jack-0 <github.ay8yf@slmail.me>
2022-10-26 23:06:48 +0100

    hosky support -_-

--- src/lib/delegators/delegators.svelte
+++ src/lib/delegators/delegators.svelte
@@ -1,28 +1,55 @@
 <script lang="ts">
-  import { laceToAdaString, shortenStakeAddr } from "../../util/utils";
+  import { formatNumber, laceToAdaString, shortenStakeAddr } from "../../util/utils";
   import { getDelegatorAssets, getDelegators } from "../../data/koios";
   import cf from "../../../public/cf.json";
   import type { DelegatorInfo } from "src/types/types";
   import Adahandle from "../../assets/icons/adahandle.svelte";
+  let hoskyLogo = 'images/hosky.png'
   let loading = true
   let delegators: DelegatorInfo[] = [];
+  let unfilteredDelegators: DelegatorInfo[] = []; // for hosky...
+
   (async () => {
-    delegators = await getDelegators();
+    delegators = await getDelegators()
+      .then((d)=>{return d.sort((a, b) => b.lace - a.lace)});
     let loading = false;
-    console.log("boo");
-    delegators = await getDelegatorAssets(delegators);
-    delegators = delegators;
+    delegators = await getDelegatorAssets(delegators)
+      .then((d)=>{return d.sort((a, b) => b.lace - a.lace)});
   })();
+
+  let showHosky = false;
+  function handleHosky(){
+    showHosky = !showHosky;
+    if(showHosky){
+      unfilteredDelegators = delegators;
+      let hoskyHolders = delegators.filter(x => x.hosky).sort((a, b) => b.hosky - a.hosky);
+      delegators = hoskyHolders;
+    } else {
+      delegators = unfilteredDelegators;
+    }
+  }
+
 </script>
 
 
 <div id="delegators">
   {#if loading && delegators.length === 0}
-    <p class="text text-md text-center p-2">Loading for delegators...</p>
+    <p class="text text-xl text-center p-2">Loading delegators...</p>
     <progress class="progress mx-auto"/>
+  {:else}
+    <!-- Hosky toggle -->
+    <div class="w-12 my-auto mx-auto mt-4 my-4">
+      <button on:click={handleHosky}>
+        {#if showHosky}
+        <img src={hoskyLogo} alt="Hosky token logo. A pixel art of a Husky dog" class="hoskyOn"/>
+        {:else}
+        <img src={hoskyLogo} alt="Hosky token logo. A pixel art of a Husky dog" class="hoskyOff"/>
+        {/if}
+      </button>
+    </div>
   {/if}
   <div class="grid gap-x-4 gap-y-2 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
-    {#each delegators.sort((a, b) => b.lace - a.lace) as delegator}
+    {#each delegators as delegator}
       {#if delegator.lace > 0}
         <div class="card mx-auto p-2 mt-4 shadow-lg max-w-sm bg-base-300 w-full">
           <!-- Show if foundation or pool wallet -->
@@ -30,8 +57,7 @@
             <p class="text text-center font-bold">
               🐋 Cardano Foundation
             </p>
-          {/if}
-          {#if delegator.stake_address === "stake1u8ag3gtdgtuj6hud7km4z0u0pcqvfndtq9fcckjvvc6yjdswaksu2"}
+          {:else if delegator.stake_address === "stake1u8ag3gtdgtuj6hud7km4z0u0pcqvfndtq9fcckjvvc6yjdswaksu2"}
             <p class="text text-center font-bold">
               OWNER
             </p>
@@ -48,9 +74,23 @@
             </p>
           {/if}
           
-          <p class="text text-center">
-            {laceToAdaString(delegator.lace.toString())}
-          </p>
+          {#if !showHosky}
+            <p class="text text-center">
+              {laceToAdaString(delegator.lace.toString())}
+            </p>
+          {/if}
+          
+          <!-- Hosky toggle -->
+          {#if showHosky && delegator.hosky}
+          <div class="flex flex-col mx-auto my-auto p-2">
+            <div class="w-10 my-auto mx-auto">
+              <img src={hoskyLogo} alt="Hosky token logo. A pixel art of a Husky dog"/>
+            </div>
+            <p class="text text-center p-1">
+              {formatNumber(delegator.hosky.toString())}
+            </p>
+          </div>
+          {/if}
 
         </div>
       {/if}
@@ -59,6 +99,15 @@
 </div>
 
 <style>
+
+  .hoskyOn {
+      filter: drop-shadow(0px 0px 15px lightblue)
+  }
+
+  .hoskyOff{
+    filter: saturate(0%);
+  }
+
   .blend {
     background: linear-gradient(-90deg, #fe706d, #fedf6d);
     background-size: 200% 100%;
