b5fb830b6ee9524e7515faef78fe18c2bd28c7dc
jack <jack.7e@protonmail.com>
2022-10-26 20:01:09 +0100

    delegators loading

---
+++ src/lib/delegators/delegators.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import { laceToAdaString, shortenStakeAddr } from "../../util/utils";
+  import { getDelegatorAssets, getDelegators } from "../../data/koios";
+  import cf from "../../../public/cf.json";
+  import type { DelegatorInfo } from "src/types/types";
+  import Adahandle from "../../assets/icons/adahandle.svelte";
+  let loading = true
+  let delegators: DelegatorInfo[] = [];
+  (async () => {
+    delegators = await getDelegators();
+    let loading = false;
+    console.log("boo");
+    delegators = await getDelegatorAssets(delegators);
+    delegators = delegators;
+  })();
+</script>
+
+
+<div id="delegators">
+  {#if loading && delegators.length === 0}
+    <p class="text text-md text-center p-2">Loading for delegators...</p>
+    <progress class="progress mx-auto"/>
+  {/if}
+  <div class="grid gap-x-4 gap-y-2 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
+    {#each delegators.sort((a, b) => b.lace - a.lace) as delegator}
+      {#if delegator.lace > 0}
+        <div class="card mx-auto p-2 mt-4 shadow-lg max-w-sm bg-base-300 w-full">
+          <!-- Show if foundation or pool wallet -->
+          {#if cf.includes(delegator.stake_address)}
+            <p class="text text-center font-bold">
+              🐋 Cardano Foundation
+            </p>
+          {/if}
+          {#if delegator.stake_address === "stake1u8ag3gtdgtuj6hud7km4z0u0pcqvfndtq9fcckjvvc6yjdswaksu2"}
+            <p class="text text-center font-bold">
+              OWNER
+            </p>
+          {/if}
+          
+          <!-- Show handle or shortened stake address -->
+          {#if delegator.handles && delegator.handles.length}
+          <p class="text text-center text-lg font-bold">
+            <span class="inline-block h-4"><Adahandle class="align"/></span> {delegator.handles[0]}
+          </p>
+          {:else}
+            <p class="text text-center text-lg font-bold text-primary">
+              {shortenStakeAddr(delegator.stake_address)}
+            </p>
+          {/if}
+          
+          <p class="text text-center">
+            {laceToAdaString(delegator.lace.toString())}
+          </p>
+
+        </div>
+      {/if}
+    {/each}
+  </div>
+</div>
+
+<style>
+  .blend {
+    background: linear-gradient(-90deg, #fe706d, #fedf6d);
+    background-size: 200% 100%;
+    animation: gradient 6s ease infinite;
+  }
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+</style>
